43f16bffab14b936678398614e369a3c44cecdfc
thichcarmoney2k7 <25023436@vnu.edu.vn>
2026-04-27 09:41:36 +0700

    "Refactoring using path.get() to fix multi OS"

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -1,13 +1,15 @@
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PathTest {
     @Test
     public void testFileHardcodedPath() {
-        String path = "src\\main\\resources\\data.txt";
-        File file = new File(path);
+        Path path = Paths.get("src", "main", "resources", "data.txt");
+        File file = path.toFile();
 
-        assertTrue(file.exists(), "File exists, But the path might be wrong on this operating system.!");
+        assertTrue(file.exists(), "File should be existed on all operating system!");
     }
 }
 
